bd2a427a639b594590f70af20e3e7d42ce8ad22b
Oleksandr Romanchuk <novator0511@gmail.com>
2023-05-10 16:13:14 +0300

    update APP.js with Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import SharedLayout from 'SharedLayout/SharedLayout';
+import AddPetPage from 'pages/AddPetPage/AddPetPage';
+import LoginPage from 'pages/LoginPage/LoginPage';
+import MainPage from 'pages/MainPage/MainPage';
+import NewsPage from 'pages/NewsPage/NewsPage';
+import NoticesPage from 'pages/NoticesPage/NoticesPage';
+import OurFriendsPage from 'pages/OurFriendsPage/OurFriendsPage';
+import RegisterPage from 'pages/RegisterPage/RegisterPage';
+import UserPage from 'pages/UserPage/UserPage';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => {
+export const App = () => {
   return (
     <Routes>
-      <Route path="/"></Route>
+      <Route path="/" element={<SharedLayout />}></Route>
+      <Route path="main" element={<MainPage />} />
+      <Route path="notices/:categoryName" element={<NoticesPage />} />
+      <Route path="friends" element={<OurFriendsPage />} />
+      <Route path="register" element={<RegisterPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="user" element={<UserPage />} />
+      <Route path="add-pet" element={<AddPetPage />} />
+      <Route path="news" element={<NewsPage />} />
     </Routes>
   );
 };
-
-export default App;
