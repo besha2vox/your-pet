e21c8d00e061ee0ed86d7d815836891ca4b6c569
Serhii Revva <besha2vox@gmail.com>
2023-05-12 17:07:24 +0300

    fix axios.basicURL

--- src/redux/friends/operations.js
+++ src/redux/friends/operations.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-});
-
 export const fetchFriends = createAsyncThunk(
   'friends/fetchFriends',
   async (_, { rejectWithValue }) => {
     try {
-      const { data } = await instance.get('/api/friends');
+      const { data } = await axios.get('/api/friends');
       return data.result;
     } catch (error) {
       return rejectWithValue(error.message);
