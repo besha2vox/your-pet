feb7bd27df6b74caca901dd35412fbf8b6320cf8
Serhii Revva <besha2vox@gmail.com>
2023-05-14 07:27:51 +0300

    delete some imports for good deploy

--- src/shared/components/AddPetCard/MoreInfoForm/MoreInfoForm.jsx
+++ src/shared/components/AddPetCard/MoreInfoForm/MoreInfoForm.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { ErrorMessage } from 'formik';
 import PropTypes from 'prop-types';
 
--- src/shared/components/AddPetCard/MoreInfoForm/MoreInfoForm.styled.jsx
+++ src/shared/components/AddPetCard/MoreInfoForm/MoreInfoForm.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ErrorMessage, Field } from 'formik';
+import { Field } from 'formik';
 import {
   AddFormInput,
   AddFormLabel,
