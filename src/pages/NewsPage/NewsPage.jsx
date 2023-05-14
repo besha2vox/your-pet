986915724f0f4817d22cd3775ba9220a6a22d29a
NataliiaVlasenko <108278401+NataliiaVlasenko@users.noreply.github.com>
2023-05-14 10:13:45 +0300

    add search function

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -7,9 +7,8 @@ import Container from 'shared/components/Container/Container';
 //import Section from 'shared/components/Section/Section';
 
 import { Title } from './NewsPage.styled';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchNews } from 'redux/news/operations';
+import { fetchNews, fetchNewsByQuery2} from 'redux/news/operations';
 
 import { getAllNews, getHintsh } from 'redux/news/selectors';
 
@@ -19,69 +18,26 @@ const NewsPage = () => {
   const data = useSelector(getAllNews);
   const hints = useSelector(getHintsh);
 
-  console.log(hints);
+  //console.log(hints);
 
   useEffect(() => {
     const fetchAllNews = async () => await dispatch(fetchNews());
     fetchAllNews();
   }, [dispatch]);
 
-
-
-  
-  const [filter, setFilter] = useState('');
-   const [inputValue, setInputValue] = useState(false);
-
-  const handleChange = event => {
-    // console.log(filter.length);
-    setFilter(event.currentTarget.value);
-  };
-
-  useEffect(() => {
-    filter.length > 0 ? setInputValue(true) : setInputValue(false);
-  }, [filter.length]);
-
-  const resetInput = event => {
-    setFilter('');
-  };
-
-  function filterNews() {
-    if (!filter) {
-      return data;
-    }
-    const normalizedFilter = filter.toLocaleLowerCase();
-    const filterlist = data.filter(news => {
-      return news.title.toLocaleLowerCase().includes(normalizedFilter);
-    });
-    //console.log(filterlist);
-    if (filterlist.length === 0) {
-      Notify.warning('Write a correct request');
-    }
-    return filterlist;
+  const onSearch = (searchQuery) => {
+    console.log(searchQuery);
+    const fetchNewsByQuery = async () => await dispatch(fetchNewsByQuery2(searchQuery));
+    fetchNewsByQuery(searchQuery);
   }
-
-  // <NewsFilter
-  //       input={filter}
-  //       onChange={handleChange}
-  //       resetInput={resetInput}
-  //       inputValue={inputValue}
-  //     />
-
-
-const onSearch =() => {
-
-
-
-}
-
 
   return (
     <Container>
       <Title> News</Title>
-      <NoticesSearch onFormSubmit = {onSearch()}>
+      <NoticesSearch onFormSubmit = {onSearch}>
 
       </NoticesSearch>
-      <NewsList data={filterNews()} />
+      <NewsList data={data} />
     </Container>
   );
 };
