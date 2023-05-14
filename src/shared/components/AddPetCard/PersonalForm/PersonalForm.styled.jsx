795fd3704ff34003d339a84fb2d95ec3e8c982cb
Serhii Revva <besha2vox@gmail.com>
2023-05-14 07:48:55 +0300

    delete some imports for good deploy

--- src/shared/components/AddPetCard/PersonalForm/PersonalForm.styled.jsx
+++ src/shared/components/AddPetCard/PersonalForm/PersonalForm.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ErrorMessage, Field } from 'formik';
+import { Field } from 'formik';
 
 export const PersonalFormWrapper = styled.div`
   display: flex;
