afae03850774ebbd8aba48a495539b7f32ef05e0
Hanna Kostiana <anna.kostianaia@gmail.com>
2023-05-18 15:58:02 +0300

    form validation

--- src/shared/components/UserPageComponents/PetsList/PetsList.jsx
+++ src/shared/components/UserPageComponents/PetsList/PetsList.jsx
@@ -1,1 +1,41 @@
+import PetsItem from '../PetsItem/PetsItem';
+// import { useState } from "react";
 
+const pets = [
+  {
+    _id: '1',
+    petOwnerId: '1',
+    category: 'my pet',
+    avatarURL:
+      'https://drive.google.com/file/d/1ptVv5bfpj-szFF92jW9SMO6NxYG7pI6n/view',
+    name: 'Jack',
+    birthday: '22.04.2018',
+    breed: 'Persian cat',
+    comments:
+      "Jack is a gray Persian cat with green eyes. He loves to be pampered and groomed, and enjoys playing with toys. Although a bitshy, he's a loyal and affectionate lap cat.",
+  },
+  {
+    _id: '2',
+    petOwnerId: '2',
+    category: 'my pet',
+    avatarURL:
+      'https://drive.google.com/file/d/194BoAxQPJoUETVOyLhIwy7adHdfsxb-S/view',
+    name: 'Jack',
+    birthday: '22.04.2018',
+    breed: 'Persian cat',
+    comments:
+      "Comments:  Jack is a handsome Basenji with short, shiny red fur and perky ears. He's an active and intelligent dog that loves to explore and play. Jack is independent and strong-willed, but also affectionate and loyal to his family. He's a curious and energetic companion that brings joy and adventure to his family's life.",
+  },
+];
+
+const PetsList = () => {
+  return (
+    <ul>
+      {pets.map(pet => (
+        <PetsItem key={pet._id} pet={pet} ava={pet.avatarURL} />
+      ))}
+    </ul>
+  );
+};
+
+export default PetsList;
--- src/shared/components/UserPageComponents/UserDataForm/UserDataForm.jsx
+++ src/shared/components/UserPageComponents/UserDataForm/UserDataForm.jsx
@@ -1,1 +1,152 @@
+import UserDataItem from '../UserDataItem/UserDataItem';
+import Logout from 'shared/components/Logout/Logout';
+import { useState, useEffect } from 'react';
+import fields from './fields';
+import useForm from '../../hooks/useForm';
+import { UserForm } from './UserDataForm.styled';
+import { Formik, ErrorMessage } from 'formik';
+import { selectUser } from "redux/auth/selectors";
+import { useSelector } from "react-redux"
+import * as Yup from 'yup';
 
+const cityRegex = /^(?:(?:[a-zA-Zа-яА-ЯіІїЇєЄ]+(?:[.'’‘`-][a-zA-Zа-яА-ЯіІїЇєЄ]+)*)\s*)+$/;
+const nameRegex = /^[a-zA-Zа-яА-ЯІіїЇґҐ\s-]+$/;
+const birthdayRegex = /^([0-2]\d|3[0-1])\.(0\d|1[0-2])\.\d{4}$/;
+
+const validationSchema = Yup.object({
+  name: Yup.string().matches(nameRegex, 'Enter your name'),
+  email:  Yup.string().email('Invalid email'),
+  birthday: Yup.string().matches(birthdayRegex, 'Invalid date format, expected dd.mm.yyyy'),
+  phone: Yup.string().matches(/^\+380\d{9}$/, 'Incorrect phone number format'),
+  city: Yup.string().matches(cityRegex, 'Enter the city in the format "Brovary", "Kyiv", "Akhtyrka", "Sumy"')
+});
+
+
+const UserDataForm = ({onSubmit}) => {
+  const user = useSelector(selectUser);
+
+  const initialState = {
+    name: '',
+    email: '',
+    birthday: '',
+    phone: '',
+    city: '',
+  };
+
+  const { state, handleChange, handleSubmit } = useForm({ initialState, onSubmit });
+  const { name, email, birthday, phone, city } = state;
+
+  const [nameIsActive, setNameIsActive] = useState(true);
+  const [emailIsActive, setEmailIsActive] = useState(true);
+  const [birthdayIsActive, setBirthdayIsActive] = useState(true);
+  const [phoneIsActive, setPhoneIsActive] = useState(true);
+  const [cityIsActive, setCityIsActive] = useState(true);
+
+  useEffect(() => {
+    if (user) {
+      setNameIsActive(true);
+      setEmailIsActive(true);
+      setBirthdayIsActive(true);
+      setPhoneIsActive(true);
+      setCityIsActive(true);
+    }
+  }, [user]);
+  
+  const countActiveButtons = () => {
+    let count = 0;
+    if (nameIsActive) count++;
+    if (emailIsActive) count++;
+    if (birthdayIsActive) count++;
+    if (phoneIsActive) count++;
+    if (cityIsActive) count++;
+    
+    if (count === 5) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const handleClick = (name) => {
+    if (countActiveButtons()) {
+      switch (name) {
+        case "name":
+          setNameIsActive(false);
+          break;
+        case "email":
+          setEmailIsActive(false);
+          break;
+        case "birthday":
+          setBirthdayIsActive(false);
+          break;
+        case "phone":
+          setPhoneIsActive(false);
+          break;
+        case "city":
+          setCityIsActive(false);
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+
+  return (
+    <Formik
+      initialValues={initialState}
+      validationSchema={validationSchema}
+    >
+      <UserForm>
+        <UserDataItem
+          value={name}
+          handleChange={handleChange}
+          {...fields.name}
+          name="name"
+          isdisabled={nameIsActive}
+          handleClick={handleClick}
+          handleSubmit={handleSubmit}
+        />
+        <UserDataItem
+          value={email}
+          handleChange={handleChange}
+          {...fields.email}
+          name="email"
+          isdisabled={emailIsActive}
+          handleClick={handleClick}
+          handleSubmit={handleSubmit}
+        />
+        <UserDataItem
+          value={birthday}
+          handleChange={handleChange}
+          {...fields.birthday}
+          name="birthday"
+          isdisabled={birthdayIsActive}
+          handleClick={handleClick}
+          handleSubmit={handleSubmit}
+        />
+
+        <UserDataItem
+          value={phone}
+          handleChange={handleChange}
+          {...fields.phone}
+          name="phone"
+          isdisabled={phoneIsActive}
+          handleClick={handleClick}
+          handleSubmit={handleSubmit}
+        />
+        <UserDataItem
+          value={city}
+          handleChange={handleChange}
+          {...fields.city}
+          name="city"
+          isdisabled={cityIsActive}
+          handleClick={handleClick}
+          handleSubmit={handleSubmit}
+        />
+      </UserForm>
+    </Formik>
+  );
+};
+
+export default UserDataForm;
