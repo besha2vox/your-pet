117f6bc43a7184bb83d385348810d2f8b4b03d1c
Hanna Kostiana <anna.kostianaia@gmail.com>
2023-05-18 15:52:51 +0300

    change

--- src/shared/components/UserPageComponents/PetsData/PetsData.jsx
+++ src/shared/components/UserPageComponents/PetsData/PetsData.jsx
@@ -1,19 +1,1 @@
-import PetsList from '../PetsList/PetsList';
-// import { MyPetCard } from './PetsData.styled';
 
-const PetsData = () => {
-  return (
-    <>
-      {/* <button
-      // onClick={Navigate}
-      >
-        Add pet
-      </button> */}
-      {/* <MyPetCard> */}
-      <PetsList />
-      {/* </MyPetCard> */}
-    </>
-  );
-};
-
-export default PetsData;
