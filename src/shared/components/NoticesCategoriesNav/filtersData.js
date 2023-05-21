d3c728db0d6aadfce6a934dcabd2e4fe2faad8f3
Oleksandr Romanchuk <novator0511@gmail.com>
2023-05-21 20:57:38 +0300

    Create filtersData.js

---
+++ src/shared/components/NoticesCategoriesNav/filtersData.js
@@ -0,0 +1,7 @@
+export const filters = [
+  { filter: 'sell', path: 'sell' },
+  { filter: 'lost/found', path: 'lost-found' },
+  { filter: 'in good hands', path: 'in-good-hands' },
+  { filter: 'favorite ads', path: 'favorite' },
+  { filter: 'my ads', path: 'my-pets' },
+];
