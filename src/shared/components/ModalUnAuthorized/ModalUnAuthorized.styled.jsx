b48b41718ca0b8900afc372c1429457f2597daeb
Oleksandr Romanchuk <novator0511@gmail.com>
2023-05-22 12:03:38 +0300

    fix unathorizedModal

--- src/shared/components/ModalUnAuthorized/ModalUnAuthorized.styled.jsx
+++ src/shared/components/ModalUnAuthorized/ModalUnAuthorized.styled.jsx
@@ -4,20 +4,25 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 250px;
+  height: 320px;
 
   padding: ${({ theme }) => theme.space[6] + 'px'};
   padding-left: ${({ theme }) => theme.space[6] + 'px'};
   padding-right: ${({ theme }) => theme.space[6] + 'px'};
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    height: 250px;
+  }
 `;
 
 const Text = styled.p`
+  width: 100%;
+
   font-family: ${({ theme }) => theme.fonts.main.bold};
   font-size: ${({ theme }) => theme.fontSizes[4]};
   text-align: center;
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    width: 321px;
     font-size: ${({ theme }) => theme.fontSizes[5]};
   }
 `;
