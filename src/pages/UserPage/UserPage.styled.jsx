e5e49a6a743454776007632e2367371a1040ad71
Makiiev Oleksandr <alsermak@gmail.com>
2023-05-22 14:27:35 +0300

    restylize components

--- src/pages/UserPage/UserPage.styled.jsx
+++ src/pages/UserPage/UserPage.styled.jsx
@@ -18,33 +18,34 @@ export const TitleWrap = styled.div`
 
 export const Title = styled.p`
   font-weight: 500;
-  font-size: 20px;
+  font-size: ${({ theme }) => theme.fontSizes[3]};
   line-height: 1.36;
   letter-spacing: 0.04em;
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    font-size: 28px;
+    font-size: ${({ theme }) => theme.fontSizes[5]};
   }
 `;
 
 export const Card = styled.div`
-  padding: 20px 8px 25px;
-  background: #ffffff;
-  box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
+  padding-top: ${({ theme }) => theme.space[2] * 5 + 'px'};
+  padding-right: ${({ theme }) => theme.space[2] * 2 + 'px'};
+  padding-bottom: ${({ theme }) => theme.space[2] * 6 + 'px'};
+  padding-left: ${({ theme }) => theme.space[2] * 2 + 'px'};
+  background: ${({ theme }) => theme.colors.classicWhite};
+  box-shadow: ${({ theme }) => theme.boxShadows.main};
   border-radius: 20px;
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
     width: 704px;
-    padding: 20px 76px 21px 20px;
+    padding-top: ${({ theme }) => theme.space[2] * 5 + 'px'};
+    padding-right: 76px;
+    padding-bottom: ${({ theme }) => theme.space[2] * 5 + 'px'};
+    padding-left: ${({ theme }) => theme.space[2] * 5 + 'px'};
     border-radius: 40px;
-    @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
-      width: 395px;
-      height: 520px;
-      padding: 20px;
-    }
-    @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
-      width: 395px;
-      height: 520px;
-      padding: 20px;
-    }
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 395px;
+    height: 520px;
+    padding: ${({theme}) => theme.space[2] * 5 + 'px'};
   }
 `;
 
