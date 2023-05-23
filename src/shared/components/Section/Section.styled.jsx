b145947adcf792c56ce55364b2423afa957f07a3
Oleksandr Romanchuk <novator0511@gmail.com>
2023-05-24 00:57:15 +0300

    Update Section.styled.jsx

--- src/shared/components/Section/Section.styled.jsx
+++ src/shared/components/Section/Section.styled.jsx
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 const Wrapper = styled.section`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
 
   width: 100vw;
+  height: 100%;
 `;
 
 export { Wrapper };
