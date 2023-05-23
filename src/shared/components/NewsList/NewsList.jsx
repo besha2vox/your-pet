600ee731bad6e22b11731c6e819f76cc31f2a5bf
Oleksandr Romanchuk <novator0511@gmail.com>
2023-05-24 00:57:32 +0300

    Update NewsList.jsx

--- src/shared/components/NewsList/NewsList.jsx
+++ src/shared/components/NewsList/NewsList.jsx
@@ -1,23 +1,27 @@
 import NewsItem from '../NewsItem/NewsItem';
-import {List} from './NewsList.styled';
+import NewsCardSkeleton from '../Skeleton/NewsCardSkeleton/NewsCardSkeleton';
+import { useSelector } from 'react-redux';
+import { selectNewsLoading } from 'redux/news/selectors';
 import { nanoid } from 'nanoid';
+import { List } from './NewsList.styled';
 
 export default function NewsList({ data }) {
+  const isLoading = useSelector(selectNewsLoading);
+  const items = data?.map(({ title, url, date, imgUrl, text }) => (
+    <NewsItem
+      key={nanoid()}
+      title={title}
+      url={url}
+      text={text}
+      date={date}
+      imgUrl={imgUrl}
+    />
+  ));
 
   return (
-    <>
-      <List>
-        {data?.map(({title, url,  date, imgUrl, text}) => (
-          <NewsItem
-            key={nanoid()}
-            title={title}
-            url={url}
-            text={text}
-            date={date}
-            imgUrl={imgUrl}
-          />
-        ))}
-      </List>
-    </>
+    <List>
+      {isLoading && <NewsCardSkeleton cards={8} />}
+      {!isLoading && items}
+    </List>
   );
 }
