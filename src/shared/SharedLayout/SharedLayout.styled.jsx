f6c37586d0657bd064a34c396fa60f72e1860670
Oleksandr Romanchuk <novator0511@gmail.com>
2023-05-24 00:56:16 +0300

    update main tag styles

--- src/shared/SharedLayout/SharedLayout.styled.jsx
+++ src/shared/SharedLayout/SharedLayout.styled.jsx
@@ -2,28 +2,19 @@ import styled from 'styled-components';
 
 const ContentWrapper = styled.div`
   min-height: 100vh;
+`;
+
+const Main = styled.main`
+  height: calc(${({ screenHeight }) => screenHeight} - 72px);
+  padding-bottom: 103px;
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    min-height: calc(100vh - 79px);
+    padding-bottom: 192px;
   }
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
-    min-height: calc(100vh - 77px);
-  }
-
-  &:after {
-    content: '';
-    display: block;
-    min-height: 0;
-
-    @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
-      min-height: 79px;
-    }
-
-    @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
-      min-height: 77px;
-    }
+    padding-bottom: 103px;
   }
 `;
 
-export { ContentWrapper };
+export { ContentWrapper, Main };
